0595731868935608a1377d624b8095b1c82afd82
Alexis Montes <montes.alexis25@gmail.com>
2021-06-09 02:02:59 -0400

    removed unnessary files

--- models/url_shorter_model.js
+++
@@ -1,9 +0,0 @@
-
-let url_shortner_schema = new mongoose.Schema({
-    url: string,
-    shortcut_path: string
-});
-
-let url_shortner_model = new mongoose.Model('url_shortner', url_shortner_schema)
-
-model.exports = url_shortner_model;
